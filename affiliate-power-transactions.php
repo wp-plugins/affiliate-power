5e9b449620d240b46be1409a5e03ea11d511ee09
JonasBreuer <JonasBreuer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-07 09:25:10 +0000

    release 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/affiliate-power/tags/1.2.1@783896 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ affiliate-power-transactions.php
@@ -0,0 +1,398 @@
+<?php
+if (!defined('ABSPATH')) die; //no direct access
+
+//for ap_download_transactions see affiliate-power.php
+add_action('wp_ajax_ap_export_csv', array('Affiliate_Power_Transactions', 'exportTransactions'));
+
+
+class Affiliate_Power_Transactions {
+
+	static public function transactionsPage() {
+		
+		$transactionList = new Affiliate_Power_Transaction_List();
+		$transactionList->prepare_items();
+		
+		$options = get_option('affiliate-power-options');
+		
+		//Check Licence
+		if (isset($options['licence-key'])) {
+			echo '<div class="error"><p><strong>'.__('You entered a valid licence key but you did not download the premium version yet. Please go to the a href="update-core.php">Update Page</a> and update to the premium version. It can take up to 5 minutes until WordPress notifies you about the new version.', 'affiliate-power').'</strong></p></div>';
+		}
+		
+		//Infotext
+		$meta_options = get_option('affiliate-power-meta-options');
+		if (isset($meta_options['infotext']) && $meta_options['hide-infotext'] == 0) {
+			echo '<div class="updated">'.$meta_options['infotext'].'</div>';
+		}
+		
+		?>
+		<div class="wrap">
+			
+			<div class="icon32" style="background:url(<?php echo plugins_url('affiliate-power/img/affiliate-power-36.png'); ?>) no-repeat;"><br/></div>
+			<?php _e ('<h2>Affiliate Power Sales</h2>', 'affiliate-power'); ?>
+			
+			<form id="movies-filter" method="get">
+				<input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+				<?php $transactionList->display() ?>
+			</form>
+			
+			<input type="button" style="float:left; width:170px;" id="button_download_transactions" value="<?php _e('Update Sales', 'affiliate-power'); ?>" /><span class="spinner" id="spinner1" style="float:left;"></span><br />
+			<input type="button" style="float:left; width:170px; clear:left;" id="button_export_csv" value="<?php _e('CSV/Excel Download', 'affiliate-power'); ?>" /><span class="spinner" id="spinner2" style="float:left;"></span>
+			
+			<script type="text/javascript">
+				jQuery(document).ready(function($) {
+				
+					$("#button_download_transactions").bind("click", function(e){
+						$(this).val('<?php _e('Please wait', 'affiliate-power'); ?>...');
+						$('#spinner1').css('display', 'block');
+						$.post(ajaxurl, { action: 'ap_download_transactions', nonce: '<?php echo wp_create_nonce( 'affiliate-power-download-transactions' ) ?>' }, function(response) {
+							location.reload();
+						});
+					});
+					
+					$("#button_export_csv").bind("click", function(e){
+						$(this).val('<?php _e('Please wait', 'affiliate-power'); ?>...');
+						$('#spinner2').css('display', 'block');
+						$.post(ajaxurl, { action: 'ap_export_csv', nonce: '<?php echo wp_create_nonce( 'affiliate-power-export-csv' ) ?>'}, function(response) {
+							$("#button_export_csv").val('<?php _e('CSV/Excel Download', 'affiliate-power'); ?>');
+							$("body").append("<iframe src='<?php echo plugins_url( "affiliate-power/csv-download.php", dirname(__FILE__ )); ?>' style='display: none;' ></iframe>")
+							$('#spinner2').css('display', 'none');
+
+						});
+					});
+					
+				});
+			</script>
+			
+		</div>
+		<?php
+	}
+	
+	
+	public static function exportTransactions() {
+	
+		check_ajax_referer( 'affiliate-power-export-csv', 'nonce' );
+		
+		global $wpdb;
+		$csv_content = __('Id;Date;Network;Merchant;Type;Price;Commission;Status;PostId;PostName', 'affiliate-power') . "\r\n";
+		
+		$sql = $wpdb->prepare('
+			SELECT 
+				'.$wpdb->prefix.'ap_transaction.ap_transactionID,
+				'.$wpdb->prefix.'ap_transaction.Date,
+				date_format('.$wpdb->prefix.'ap_transaction.Date, "%%d.%%m.%%Y %%T") AS datetime_de,
+				'.$wpdb->prefix.'ap_transaction.network,
+				'.$wpdb->prefix.'ap_transaction.ProgramTitle,
+				'.$wpdb->prefix.'ap_transaction.Transaction,
+				'.$wpdb->prefix.'ap_transaction.Price,
+				'.$wpdb->prefix.'ap_transaction.Commission,
+				'.$wpdb->prefix.'ap_transaction.TransactionStatus,
+				'.$wpdb->prefix.'posts.ID AS postID,
+			    '.$wpdb->posts.'.post_title
+			FROM '.$wpdb->prefix.'ap_transaction
+			LEFT JOIN '.$wpdb->prefix.'ap_clickout
+			ON '.$wpdb->prefix.'ap_transaction.SubId = '.$wpdb->prefix.'ap_clickout.ap_clickoutID
+			LEFT JOIN '.$wpdb->posts.'
+			ON '.$wpdb->prefix.'ap_clickout.postID = '.$wpdb->prefix.'posts.ID
+			WHERE '.$wpdb->prefix.'ap_transaction.SubId >= 1000000
+			UNION
+			SELECT 
+				'.$wpdb->prefix.'ap_transaction.ap_transactionID,
+				'.$wpdb->prefix.'ap_transaction.Date,
+				date_format('.$wpdb->prefix.'ap_transaction.Date, "%%d.%%m.%%Y %%T") AS datetime_de,
+				'.$wpdb->prefix.'ap_transaction.network,
+				'.$wpdb->prefix.'ap_transaction.ProgramTitle,
+				'.$wpdb->prefix.'ap_transaction.Transaction,
+				'.$wpdb->prefix.'ap_transaction.Price,
+				'.$wpdb->prefix.'ap_transaction.Commission,
+				'.$wpdb->prefix.'ap_transaction.TransactionStatus,
+				'.$wpdb->prefix.'posts.ID AS postID,
+			    '.$wpdb->posts.'.post_title
+			FROM '.$wpdb->prefix.'ap_transaction
+			LEFT JOIN '.$wpdb->posts.'
+			ON '.$wpdb->prefix.'ap_transaction.SubId = '.$wpdb->prefix.'posts.ID
+			WHERE '.$wpdb->prefix.'ap_transaction.SubId < 1000000
+			ORDER by Date ASC
+		');
+		$transactions = $wpdb->get_results($sql, ARRAY_A);
+		
+		foreach ($transactions as $transaction) {
+			$transaction['Price'] = str_replace('.', ',', $transaction['Price']);
+			$transaction['Commission'] = str_replace('.', ',', $transaction['Commission']);
+			unset($transaction['Date']); //this was just for order by
+			$csv_content .= implode(';', $transaction) . "\r\n";
+		}
+		
+		if (!session_id()) session_start();
+		$_SESSION['affiliate-power-csv'] = $csv_content;
+		
+		die();
+	}
+	
+}
+
+	
+	
+if(!class_exists('WP_List_Table')){
+    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+
+
+class Affiliate_Power_Transaction_List extends WP_List_Table {
+
+	
+
+	function __construct(){
+		global $status, $page;
+                
+        //Set parent defaults
+        parent::__construct( array(
+            'singular'  => __('Sale', 'affiliate-power'),     //singular name of the listed records
+            'plural'    => __('Sales', 'affiliate-power'),    //plural name of the listed records
+            'ajax'      => false        //does this table support ajax?
+        ) ); 
+    }
+	
+	
+	function column_default($item, $column_name){
+	
+		switch ($column_name) {
+		
+			case 'Transaction' :
+				if ($item['Transaction'] == 'S') $value = 'Sale';
+				elseif ($item['Transaction'] == 'L') $value = 'Lead';
+				elseif ($item['Transaction'] == 'K') $value = 'Kombi';
+				else $value = $item['Transaction'];
+				break;
+		
+			case 'Price' :
+				if ($item['Price'] == 0) $value = '---';
+				else {
+					$value = number_format($item['Price'], 2, ',', '.');
+					$value .= ' €';
+				}
+				break;
+				
+				
+			case 'Commission' :
+				$value = number_format($item['Commission'], 2, ',', '.');
+				$value .= ' €';
+				break;
+				
+			case 'TransactionStatus' :
+				if ($item['TransactionStatus'] == 'Cancelled') $value = _x('Cancelled', 'single', 'affiliate-power');
+				elseif ($item['TransactionStatus'] == 'Confirmed') $value = _x('Confirmed', 'single', 'affiliate-power');
+				else $value = _x('Open', 'single', 'affiliate-power');
+				break;
+				
+			case 'post_title' :
+				$options = get_option('affiliate-power-options');
+				if ($options['add-sub-ids'] === 0) {
+					$value = $item['postID'];
+				}
+				else {
+					if ($item['post_title'] == '') {
+						$value = __('- unknown -', 'affiliate-power');
+						break;
+					} 
+					
+					$permalink = get_permalink($item['postID']);
+					
+					$actions = array(
+						'view'    => sprintf('<a href="%s" target="_blank">'.__('View', 'affiliate-power').'</a>',$permalink),
+						'edit'    => sprintf('<a href="post.php?post=%d&action=%s">'.__('Edit', 'affiliate-power').'</a>',$item['postID'],'edit')
+					);
+					$value = sprintf('%1$s %2$s',
+						/*$1%s*/ $item['post_title'],
+						/*$2%s*/ $this->row_actions($actions)
+					);
+				}
+				break;
+			
+			case 'referer' :
+				$value = __('<a href="http://www.affiliatepowerplugin.com/premium/" target="_blank">Only in the premium version</a>', 'affiliate-power');
+				break;
+				
+				
+			default :
+				$value = $item[$column_name];
+		}
+	
+		if ($item['TransactionStatus'] == 'Cancelled') $color = '#FF0000';
+		elseif ($item['TransactionStatus'] == 'Confirmed') $color = 'green';
+		else $color = '#666666';
+		
+		$output = '<span style="color:'.$color.';">'.$value.'</span>';
+		return $output;
+	
+    }
+
+
+	
+	
+	function get_columns(){
+		$options = get_option('affiliate-power-options');
+		if ($options['add-sub-ids'] !== 0) $post_title_text = __('Post', 'affiliate-power');
+		else $post_title_text = __('SubId', 'affiliate-power');
+
+        $columns = array(
+            'germanDate'     => __('Date', 'affiliate-power'),
+            'network'    => __('Network', 'affiliate-power'),
+            'ProgramTitle'  => __('Merchant', 'affiliate-power'),
+			'Transaction' => __('Type', 'affiliate-power'),
+			'Price' => __('Price', 'affiliate-power'),
+			'Commission'  => __('Commission', 'affiliate-power'),
+			'TransactionStatus' => __('Status', 'affiliate-power'),
+			'post_title' => $post_title_text,
+			'referer' => __('Referer', 'affiliate-power'),
+			'landing_page' => __('Landing Page', 'affiliate-power'),
+        );
+        return $columns;
+    }
+	
+	
+	function get_sortable_columns() {
+        $sortable_columns = array(
+            'germanDate'     => array('Date',true),     //true means its already sorted
+            'network'    => array('network',false),
+            'ProgramTitle'  => array('ProgramTitle',false),
+			'Transaction'  => array('Transaction',false),
+			'Price'  => array('Price',false),
+			'Commission'  => array('Commission',false),
+			'TransactionStatus'  => array('TransactionStatus',false),
+			'post_title'  => array('post_title',false),
+			'referer' => array('referer',false)
+			
+        );
+        return $sortable_columns;
+    }
+	
+	
+	
+	
+	function prepare_items() {
+        
+        $per_page = 20;
+        
+        
+        /**
+         * REQUIRED. Now we need to define our column headers. This includes a complete
+         * array of columns to be displayed (slugs & titles), a list of columns
+         * to keep hidden, and a list of columns that are sortable. Each of these
+         * can be defined in another method (as we've done here) before being
+         * used to build the value for our _column_headers property.
+         */
+        $columns = $this->get_columns();
+        $hidden = array();
+        $sortable = $this->get_sortable_columns();
+        
+        
+        /**
+         * REQUIRED. Finally, we build an array to be used by the class for column 
+         * headers. The $this->_column_headers property takes an array which contains
+         * 3 other arrays. One for all columns, one for hidden columns, and one
+         * for sortable columns.
+         */
+        $this->_column_headers = array($columns, $hidden, $sortable);
+        
+               
+        
+        
+		$orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'Date'; //If no sort, default to date, todo: whitelist
+        $order = (isset($_REQUEST['order']) && $_REQUEST['order'] == 'asc') ?  'asc' : 'desc'; //If no order, default to asc
+			
+        global $wpdb;
+	
+		$sql = ' 
+		SELECT '.$wpdb->prefix.'ap_transaction.ap_transactionID,
+			   '.$wpdb->prefix.'ap_transaction.network,
+			   '.$wpdb->prefix.'ap_transaction.Date,
+			   date_format('.$wpdb->prefix.'ap_transaction.Date, "%d.%m.%Y - %T") AS germanDate,
+			   '.$wpdb->prefix.'ap_transaction.SubId,
+			   '.$wpdb->prefix.'ap_transaction.ProgramTitle,
+			   '.$wpdb->prefix.'ap_transaction.Transaction,
+			   '.$wpdb->prefix.'ap_transaction.Price,
+			   '.$wpdb->prefix.'ap_transaction.Commission,
+			   '.$wpdb->prefix.'ap_transaction.Confirmed,
+			   '.$wpdb->prefix.'ap_transaction.TransactionStatus,
+			   "- unknown -" as referer,
+			   '.$wpdb->prefix.'posts.ID AS postID,
+			   '.$wpdb->posts.'.post_title
+		FROM '.$wpdb->prefix.'ap_transaction
+		LEFT JOIN '.$wpdb->prefix.'ap_clickout
+		ON '.$wpdb->prefix.'ap_transaction.SubId = '.$wpdb->prefix.'ap_clickout.ap_clickoutID
+		LEFT JOIN '.$wpdb->posts.'
+		ON '.$wpdb->prefix.'ap_clickout.postID = '.$wpdb->prefix.'posts.ID
+		WHERE '.$wpdb->prefix.'ap_transaction.SubId >= 1000000
+		UNION
+		SELECT '.$wpdb->prefix.'ap_transaction.ap_transactionID,
+			   '.$wpdb->prefix.'ap_transaction.network,
+			   '.$wpdb->prefix.'ap_transaction.Date,
+			   date_format('.$wpdb->prefix.'ap_transaction.Date, "%d.%m.%Y - %T") AS germanDate,
+			   '.$wpdb->prefix.'ap_transaction.SubId,
+			   '.$wpdb->prefix.'ap_transaction.ProgramTitle,
+			   '.$wpdb->prefix.'ap_transaction.Transaction,
+			   '.$wpdb->prefix.'ap_transaction.Price,
+			   '.$wpdb->prefix.'ap_transaction.Commission,
+			   '.$wpdb->prefix.'ap_transaction.Confirmed,
+			   '.$wpdb->prefix.'ap_transaction.TransactionStatus,
+			   "- unknown -" as referer,
+			   '.$wpdb->prefix.'posts.ID AS postID,
+			   '.$wpdb->posts.'.post_title
+		FROM '.$wpdb->prefix.'ap_transaction
+		LEFT JOIN '.$wpdb->posts.'
+		ON '.$wpdb->prefix.'ap_transaction.SubId = '.$wpdb->prefix.'posts.ID
+		WHERE '.$wpdb->prefix.'ap_transaction.SubId < 1000000
+		ORDER BY '.$orderby.' '.$order;
+		
+		$transactionData = $wpdb->get_results($sql, ARRAY_A);
+        
+                
+        /**
+         * REQUIRED for pagination. Let's figure out what page the user is currently 
+         * looking at. We'll need this later, so you should always include it in 
+         * your own package classes.
+         */
+        $current_page = $this->get_pagenum();
+        
+        /**
+         * REQUIRED for pagination. Let's check how many items are in our data array. 
+         * In real-world use, this would be the total number of items in your database, 
+         * without filtering. We'll need this later, so you should always include it 
+         * in your own package classes.
+         */
+        $total_items = $wpdb->num_rows;
+        
+        
+        /**
+         * The WP_List_Table class does not handle pagination for us, so we need
+         * to ensure that the data is trimmed to only the current page. We can use
+         * array_slice() to 
+         */
+        $transactionData = array_slice($transactionData,(($current_page-1)*$per_page),$per_page);
+        
+        
+        
+        /**
+         * REQUIRED. Now we can add our *sorted* data to the items property, where 
+         * it can be used by the rest of the class.
+         */
+        $this->items = $transactionData;
+        
+        
+        /**
+         * REQUIRED. We also have to register our pagination options & calculations.
+         */
+        $this->set_pagination_args( array(
+            'total_items' => $total_items,                  //WE have to calculate the total number of items
+            'per_page'    => $per_page,                     //WE have to determine how many items to show on a page
+            'total_pages' => ceil($total_items/$per_page)   //WE have to calculate the total number of pages
+        ) );
+    }
+	
+
+}
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,265 @@
+=== Affiliate Power ===
+Contributors: JonasBreuer
+Donate link: http://www.j-breuer.de/wordpress-plugins/affiliate-power/
+Tags: affiliate, tracking, subid, sales, leads, affili, affili.net, zanox, tradedoubler, belboon, superclix, cj, commission junction, adcell
+Requires at least: 3.1
+Tested up to: 3.6.1
+Stable tag: trunk
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+
+With Affiliate Power you can analyze your Affiliate income per Post, Referer, Keyword etc.
+
+
+
+== Description ==
+
+= EN: =
+
+Affiliate Power tracks your affiliate income per post, so you can focus on things that pay! All data is saved on your server only.
+
+If you have questions, just leave a comment on the [Plugin Page](http://www.affiliatepowerplugin.com/). I'm glad to help
+
+**Premium Version**
+
+In addition to the free basic version, there is a premium version of the plugin, where you can also analyze your income per Referer, Keyword, Campaign etc. Find out more on the [Premium Page](http://www.affiliatepowerplugin.com/premium/).
+
+**Features**
+
+Features, which only exist in the premium version are *emphasized*.
+
+* Supported Networks: adcell, affili.net, commission junction, belboon, superclix, tradedoubler, zanox
+* Track income per Posts, Pages, *Referer, Keywords and URL-Parameters like utm_campaign*
+* Filter your import per website
+* Overview over all Leads and Sales
+* Automatic daily infomail on new or changes Sales
+* Export all Sales as Excel-CSV
+* Statistics over any period with income per Posts, Partner, Networks, Days, Weeks, Months, *Landing Pages, Referer, Keywords and URL-Parameters*
+* *Detail Statistics over single posts, partners, etc*
+* Sync your income with the [Webworker Dashboard](https://www.webworker-dashboard.de).
+
+= DE: =
+
+Mit Affiliate Power werden deine erzielten Affiliate-Einnahmen automatisch deinen Artikeln und Seiten zugeordnet. So kannst du herausfinden, welcher Artikel sich gelohnt hat und welcher weniger. Alle Daten werden dabei auf deinem Server ausgewertet und sind somit sicher.
+
+Bei Fragen oder Problemen hinterlasse einfach einen Kommentar auf der [Plugin Seite](http://www.j-breuer.de/wordpress-plugins/affiliate-power/). Ich helfe gerne.
+
+**Premium Version**
+
+Zusätzlich zur kostenlosen Basic-Version gibt es eine Premium-Version des Plugins. Dort können neben den Artikeln auch Besucherquellen, Keywords etc. ausgewertet werden. Mehr Infos gibt es auf der [Premium Seite](http://www.j-breuer.de/wordpress-plugins/affiliate-power-premium/).
+
+**Features**
+
+Features die nur in der Premium-Version verfügbar sind, sind *kursiv*
+
+* Aktuell verfügbare Netzwerke: adcell, affili.net, commission junction, belboon, superclix, tradedoubler, zanox
+* Automatische Zuweisung von Einnahmen zu Artikeln, Seiten, *Landing-Pages, Besucherquellen, Keywords und URL-Parametern wie utm_campaign*
+* Filterung des Imports nach Webseiten
+* Übersicht über alle Leads und Sales (ausführlich und als Dashboard-Widget)
+* Automatische tägliche Infomail bei neuen oder geänderten Leads und Sales
+* Export aller Leads und Sales als Excel-CSV
+* Statistiken über einen frei wählbaren Zeitraum sortiert nach Artikeln, Partnern, Netzwerken, Tagen, Wochen, Monaten, *Landing-Pages, Besucherquellen, Keywords und URL-Parametern*
+* *Detail-Statistiken über einzelne Artikel, Partner etc.*
+* Import der Affiliate-Einnahmen ins [Webworker Dashboard](https://www.webworker-dashboard.de).
+
+
+== Installation ==
+
+= EN: =
+
+As usual.
+
+1. Upload the directory `affiliate-power` in `/wp-content/plugins/` (or install the plugin over the plugin manager of WordPress)
+2. Activate the plugin over the plugin manager of WordPress
+3. The plugin creats it's own submenu `Affiliate Power`. In the menu item `Settings` you can enter your affiliate network data
+4. After you have entered your affiliate network data, you can download your old sales on the page `Leads/Sales` by clicking on the `Refresh transactions` button.
+5. The plugin will download your sales autoamtically once a day.
+
+
+= DE: =
+
+Wie immer.
+
+1. Lade das Verzeichnis `affiliate-power` in `/wp-content/plugins/` hoch (oder installiere das Plugin über den Plugin-Manager von Wordpress)
+2. Aktiviere das Plugin über den Plugin-Manager von Wordpress.
+3. Das Plugin erstellt ein eigenes Untermenü `Affiliate Power`. Unter `Einstellungen` kannst du das Artikel-Tracking aktivieren und die Daten der Affiliate-Netzwerke hinterlegen
+4. Im Menüpunkt `Leads/Sales` kannst du nach dem Eintrag der Netzwerk-Daten auf `Transaktionen aktualisieren` klicken, um deine bisheringen Transaktionen zu importieren
+5. Das Plugin aktualisiert künftig einmal täglich automatisch deine Transaktionen.
+
+
+
+== Frequently Asked Questions ==
+
+= EN: =
+
+**Is my Data safe?**
+
+Yes, your income data is stored only in your WordPress database. 
+
+**Are additional networks planned?**
+
+Definitely! Just leave a comment on the [Plugin Page](http://www.affiliatepowerplugin.com/) or write to me saying which network you think is missing. 
+
+**Whats ybout the Amazon Partnernet?**
+
+Unfortunately, Amazon offers no real API for downloading sales, so an intergration is very difficult. I'm working on a solution, but I can't make any promise yet.
+
+**Does the plug in work with Pretty Link?**
+
+Yes, with Pretty Link all tracking is automatically integrated. With other link cloakers only the transactions download works in the admin area but not article allocation etc. 
+
+**Can I use the plugin with its own SubId tracking?**
+
+Yes, you can use the plugin, but you must then disable the tracking option as it also makes use of SubIds. In this case, only the sales are downloaded and there is no article assignment, visitor sources, etc. Maybe it is better to remove your own SubIds? 
+
+**Is the sales data on the plugin up to date?**
+
+The plugin automatically downloads the sales once a day. You can, however, manually download the latest sales figures at any time.
+
+
+= DE: =
+
+**Sind meine Daten sicher?**
+
+Ja, alle Daten bleiben auf deinem Server.
+
+**Werden noch weitere Netzwerke hinzugefügt?**
+
+Auf jeden Fall! Hinterlasse doch einen Kommentar auf der [Plugin Seite](http://www.j-breuer.de/wordpress-plugins/affiliate-power/), welches Netzwerk dir noch fehlt.
+
+**Was ist mit dem Amazon Partnerprogramm?**
+
+Amazon bietet leider kein vernünftiges Tracking an. Daher ist eine Integration des Amazon Partnerprogramms sehr schwierig. Ich arbeite momentan an einer Lösung, kann aber noch nichts versprechen.
+
+**Funktioniert das Plugin mit einem Link-Cloaker wie Pretty Link?**
+
+Ja, bei Pretty Links wird das Artikel-Tracking automatisch integriert. Bei anderen Link-Cloakern funktioniert leider nur der Download der Transaktionen in den Adminbereich.
+
+**Kann ich das Plugin zusammen mit einem eigenen SubId Tracking benutzen**
+
+Ja, du kannst das Plugin benutzen, solltest allerdings das Artikel Tracking nicht aktivieren, da dieses ebenfalls Gebrauch von den SubIds macht.
+
+**Wie oft werden die Leads / Sales aktualisiert?**
+
+Einmal täglich findet eine automatische Aktualisierung statt. Du kannst im Backend zusätzlich jederzeit eine manuelle Aktualisierung auslösen.
+
+
+== Screenshots ==
+
+1. Income per Post
+2. Overview over your Sales
+3. Statistics (Premium-Version)
+
+== Changelog ==
+
+= 1.2.1 =
+* Fixed a bug where image affiliate links were not opened into a new window
+* Better support for WP multisite
+* Fixed cancelled sales in infomail
+* Delete old data from database
+* A few minor JS bugfixes
+
+= 1.2.0 =
+* Added Website filter for Superclix
+* Added totals in daily EMail
+* Fixed Confirmed Sales in daily Email
+* Fixed Spaces in Google Keywords
+* Added Newsletter Optin
+* Added some infotexts after x days of using the plugin
+* Fixed some minor translation issues
+
+= 1.1.0 =
+* English version
+* Integration with Webworker Dashboard
+* Daily download is now always at 3am
+* Daily mail include all sales of the day
+* Multiple ad platforms are now allowed for Belboon, Tradedoubler and Zanox
+* Little Bug Fixes and better explanations of some fields
+
+= 1.0.2 =
+* Unterstützung für verschlüsselte Adcell Links nach dem neuen Linkformat
+
+= 1.0.1 =
+* Einnahmen werden auch in der Seitenübersicht angezeigt
+* Untersützung für neues Adcell Linkformat
+* Pretty Link Pro URL Rotation gefixt
+* Mail nur bei Tagesabruf
+
+= 1.0.0 =
+* Unterstützung von Custom Post Types
+* Einbau eines eigenen Logos
+* *Einbau der Detail-Statistiken*
+* Verbesserte Erklärungstexte und Benutzerführung
+* Kleine Bugfixes
+
+= 0.7.3 =
+* Bug beim Zanox-Tracking gefixt
+
+= 0.7.2 =
+* Probleme mit bestimmten Deeplinks gefixt
+* Berücksichtigung alternativer Tradedoubler Links
+
+= 0.7.1 =
+* Bugfix: Datumsauswahl unter Chrome
+* Bugfix: Adcell Import 
+
+= 0.7.0 =
+* Adcell integriert
+* *frei definierbare URL-Parameter*
+* Schnellauswahl heute/gestern/letzte 7 Tage/letzte 30 Tage/gesamt in den Statistiken
+* Statistik nach Tagen/Wochen/Monat wieder eingebaut
+* Datum im E-Mail-Report
+* Beim ersten Sales-Download 100 Tage überprüfen 
+* Artikel-Tracking automatisch aktivieren bei Neuinstallation
+* Startseite in Statistik extra erwähnen 
+* Diverse Bugfixes und Performance-Verbesserungen
+
+= 0.6.2 =
+* Bug gefixt, durch den die SubId bei Belboon Deeplinks nicht richtig angehängt wurde
+* Performance-Verbesserung bei Sales-Übersicht und Statistik
+
+= 0.6.0 =
+* Frei wählbarer Zeitraum bei den Statistiken
+* Robusteres Tracking
+* Einführung Premium-Version
+* Code aufgeräumt
+* Kleine Bugfixes
+
+= 0.5.1 =
+* Bug gefixt, bei dem Leads und Sales bei Bestätigung auf 0€ gesetzt wurden (Hotfix)
+
+= 0.5.0 =
+* Neues Netzwerk: Commission Junction
+* Optionale Infomail bei neuen oder geänderten Leads und Sales
+* Dashboard-Widget zur Übersicht über die Einnahmen
+* CSV-Export für Excel
+* Wordpress 3.5 Kompatibilität
+* Kleine Usability-Verbesserungen
+
+= 0.4.0 =
+* Neues Netzwerk: Superclix
+* Neue Statistiken: Netzwerke, Tage, Wochen
+* Neue Spalten in der Transaktions-Übersicht: Typ (Sale oder Lead), Einkaufswert
+* Einstellungsseite überarbeitet (Untersektionen, Erklärungstexte)
+* Performance Verbesserungen
+* Kleine Bugfixes
+* Code aufgeräumt
+
+= 0.3.2 =
+* Artikel-Tracking von Zanox Deeplinks gefixt (Hotfix)
+
+= 0.3.1 =
+* Zuweisung von Belboon Sales zu Artikeln gefixt (Hotfix)
+
+= 0.3.0 =
+* Integration Belboon
+* Filter nach Webseiten
+* Pretty Link Tracking auf der Startseite gefixt
+* Kleine Bugfixes
+
+= 0.2.0 =
+* Integration in Pretty Links
+
+= 0.1.0 =
+* Erste Beta-Version
